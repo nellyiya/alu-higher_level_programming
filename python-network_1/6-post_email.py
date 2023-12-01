40a259369dd382958fdc6a390fee98749566b675
nellyiya <144996524+nellyiya@users.noreply.github.com>
2023-12-01 17:53:57 +0000

    task 6

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-"""A script that sends POST requests and displays the response"""
+"""
+Script that takes in a URL and an email address, sends a POST request to the
+passed URL with the email as a parameter, and finally displays the body
+of the response.
+
+"""
+import requests
+from sys import argv
 
 
-if __name__ == '__main__':
-    import requests
-    import sys
+if __name__ == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    req = requests.post(url, data=value)
 
-xyz = {'email': sys.argv[2]}
-    request = requests.post(sys.argv[1], data=xyz)
-    print("{}".format(request.text))
+    print(req.text)
