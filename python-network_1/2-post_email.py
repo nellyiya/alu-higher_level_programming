413b3fce299f5259795ec621fd6cf9338f345c75
nellyiya <n.iyabikoze@alustudent.com>
2023-12-01 20:35:52 +0000

    tt

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -12,4 +12,4 @@ if __name__ == '__main__':
 
     request = Request(sys.argv[1], data)
     with urlopen(request) as response:
-        print(response.read().decode('utf-8'))        
+        print(response.read().decode('utf-8'))
