248ab64c8c36e51e43e7110dc6a42dd80fe1d8e0
nellyiya <n.iyabikoze@alustudent.com>
2023-12-01 20:51:25 +0000

    tt

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""send request."""
-import urllib.request
-import urllib.error
-import sys
+"""__summary__
+- Write a Python script that takes in a URL,
+- sends a request to the URL
+- displays the body of the response (decoded in utf-8).
+"""
 
-
-def send_request():
-    """Send request."""
-    try:
-        with urllib.request.urlopen(sys.argv[1]) as re:
-            xyz = re.read()
-            print(xyz.decode("utf-8"))
-    except urllib.error.HTTPError as err:
-        print("Error code: {}".format(err.code))
 
 if __name__ == "__main__":
-    send_request()   
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as resq:
+            print(resq.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print('Error code:', e.code)   
