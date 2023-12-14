3be6daf291d523a3619fea0b0ded4728389f0ab6
nellyiya <n.iyabikoze@alustudent.com>
2023-12-14 09:07:45 -0800

    task 1,2,3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
-"""
-Prints out all values in the states tables of a database
-where name matches the argument in a safe way
-"""
+"""Script that takes in an argument and
+displays all values in the states"""
 
 
 import sys
 import MySQLdb
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
 
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
-    states = cursor.fetchall()
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3],
+        host="localhost",
+        port=3306
+    )
+    cursor = conn.cursor()
+    sql = """ SELECT * FROM states
+        WHERE name LIKE BINARY '{}'
+        ORDER BY id ASC """.format(sys.argv[4])
 
-    for state in states:
-        print(state)
+    cursor.execute(sql)
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    cursor.close()
+    conn.close()
